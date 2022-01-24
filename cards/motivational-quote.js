3fc63a94cbc3c23b8ad39428103b04011a9663b7
akanshSirohi <akanshjaat@gmail.com>
2022-01-24 17:04:52 +0530

    Removed experimental code

--- cards/motivational-quote.js
+++ cards/motivational-quote.js
@@ -2,18 +2,12 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const { generateCard } = require("../card-generator");
-const { svg2png } = require("svg-png-converter");
 
 router.get("/", (req, res) => {
   let theme = "dark_2";
-  let type = "svg";
 
   if ("theme" in req.query) {
     theme = req.query.theme;
-  }
-
-  if ("type" in req.query) {
-    type = req.query.type;
   }
 
   let quotes = fs.readFileSync("./data/motivational_quotes.json", {
@@ -28,31 +22,11 @@ router.get("/", (req, res) => {
   let quote_content = `${random_quote.quote}\n\n- ${random_quote.author}`;
 
   generateCard(quote_content, theme, (quote_card) => {
-    if (type === "img") {
-      svg2png({
-        input: quote_card.trim(),
-        encoding: "buffer",
-        format: "jpeg",
-        quality: 1,
-      })
-        .then((buffer) => {
-          res.writeHead(200, {
-            "Content-Type": "image/jpeg",
-            "Cache-Control": "public, max-age=10",
-          });
-          res.end(buffer);
-        })
-        .catch((err) => {
-          console.log(err);
-          res.json({ msg: "Error" });
-        });
-    } else {
-      res.writeHead(200, {
-        "Content-Type": "image/svg+xml",
-        "Cache-Control": "public, max-age=10",
-      });
-      res.end(quote_card);
-    }
+    res.writeHead(200, {
+      "Content-Type": "image/svg+xml",
+      "Cache-Control": "public, max-age=10",
+    });
+    res.end(quote_card);
   });
 });
 
