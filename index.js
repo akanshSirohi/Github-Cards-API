8237b7059fc872822525dea78bc84f3020e10315
Anniebhalla10 <anniebhalla16@gmail.com>
2023-10-04 12:47:41 +0200

    feat: add route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +10,7 @@ app.use("/jokes-card", require("./src/cards/joke-card"));
 app.use("/programming-quotes-card", require("./src/cards/programming-quote"));
 app.use("/motivational-quotes-card", require("./src/cards/motivational-quote"));
 app.use("/word-of-the-day-card", require("./src/cards/word_of_the_day"));
+app.use("/challenge-of-the-week-card", require("./src/cards/challenge-of-the-week"));
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
