932872c7e6d1bc93104b37ad554c37b2be1e667c
Anniebhalla10 <anniebhalla16@gmail.com>
2023-10-04 13:20:49 +0200

    fix: port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
