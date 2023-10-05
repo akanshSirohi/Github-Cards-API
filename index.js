68b861025d4ffb0aa748aa7ba69d3299c4f10ecf
sidhant-sriv <sidhant.sriv@gmail.com>
2023-10-05 14:13:21 +0530

    random route redirects to random card

--- index.js
+++ index.js
@@ -11,7 +11,21 @@ app.use("/programming-quotes-card", require("./src/cards/programming-quote"));
 app.use("/motivational-quotes-card", require("./src/cards/motivational-quote"));
 app.use("/word-of-the-day-card", require("./src/cards/word_of_the_day"));
 app.use("/challenge-of-the-week-card", require("./src/cards/challenge-of-the-week"));
-app.use("/random-card", require("./src/cards/random"));
+
+//Random route that redirects to a random route
+app.get("/random-card", (req, res) => {
+  let routes = [
+    "/jokes-card",
+    "/programming-quotes-card",
+    "/motivational-quotes-card",
+    "/word-of-the-day-card",
+    "/challenge-of-the-week-card",
+  ];
+  let randomRoute = routes[Math.floor(Math.random() * routes.length)];
+  res.redirect(randomRoute);
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
