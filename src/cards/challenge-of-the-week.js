ef53ce4ae68634bf66bd192ff0bb6c95984dcbc8
Madhura Jayashanka <madhura@Madhuras-MacBook-Air.local>
2023-10-05 23:47:41 +0530

    optimized challenge-of-the-week using best practices

--- src/cards/challenge-of-the-week.js
+++ src/cards/challenge-of-the-week.js
@@ -1,39 +1,46 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
+const fs = require("fs").promises; // Use the promise-based version of fs
 const { generateCard } = require("../card-generator");
 const { parseOptions } = require("../options-parser");
 
-router.get("/", (req, res) => {
-  let theme = "light";
+const CHALLENGE_FILE_PATH = "./src/data/challenge_of_the_week.json";
+const DEFAULT_THEME = "light";
 
-  if ("theme" in req.query) {
-    theme = req.query.theme;
+// Middleware for theme handling
+function handleTheme(req, res, next) {
+  req.theme = req.query.theme || DEFAULT_THEME;
+  next();
+}
+
+// Middleware for options parsing
+function handleOptions(req, res, next) {
+  if (req.theme === "custom") {
+    req.options = parseOptions(req.query);
   }
+  next();
+}
 
-  let options = null;
-  if (theme === "custom") {
-    options = parseOptions(req.query);
-  }
+router.get("/", handleTheme, handleOptions, async (req, res) => {
+  try {
+    const challengeData = await fs.readFile(CHALLENGE_FILE_PATH, "utf8");
+    const challengeArray = JSON.parse(challengeData);
 
-  let challenegeOfTheWeekData = fs.readFileSync("./src/data/challenge_of_the_week.json", {
-    encoding: "utf8",
-    flag: "r",
-  });
+    const randomChallenge = challengeArray[Math.floor(Math.random() * challengeArray.length)];
 
-  challenegeOfTheWeekData = JSON.parse(challenegeOfTheWeekData);
+    const challengeContent = `Challenge of the week:\n${randomChallenge.challenge}`;
 
-  let randomChallenge = challenegeOfTheWeekData[Math.floor(Math.random() * challenegeOfTheWeekData.length)];
+    const challengeCard = await generateCard(challengeContent, req.theme, req.options);
 
-  let challengeContent = `Challenge of the week:\n${randomChallenge.challenge}`;
-
-  generateCard(challengeContent, theme, options, (challengeCard) => {
     res.writeHead(200, {
       "Content-Type": "image/svg+xml",
       "Cache-Control": "public, max-age=10",
     });
     res.end(challengeCard);
-  });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
