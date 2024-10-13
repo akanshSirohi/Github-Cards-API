f43245ffc357f8454f5ee0350f3fec73e7883775
unknown <alexhparsons@gmail.com>
2024-10-13 11:41:04 -0700

    adding additional files

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const available_cards = {
   "/programming-facts-card": require("./src/cards/programming-facts"),
   "/spanish-quote-card": require("./src/cards/spanish-quote"),
   "/top-tweets-card": require("./src/cards/top-tweets"),
-  "/github-facts-card": require("./src/cards/github-facts")
+  "/github-facts-card": require("./src/cards/github-facts"),
+  "/security-tips-card": require("./src/cards/security-tips")
 };
 
 app.use(express.json());
