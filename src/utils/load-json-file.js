916ee697b9e9b5ab31ee62cbce8c230f1486f3c0
Akansh Sirohi <34837239+akanshSirohi@users.noreply.github.com>
2025-06-04 12:26:58 +0530

    return null for missing json

--- src/utils/load-json-file.js
+++ src/utils/load-json-file.js
@@ -1,7 +1,8 @@
 export async function loadJSONFile(env, key) {
   const res = await fetch(`${env.ASSETS_BASE_URL}/data/${key}`);
   if (!res.ok) {
-    throw new Error(`Mock file ${key} not found`);
+    console.error(`Data file ${key} not found`);
+    return null;
   }
   return await res.json();
 }
